a5a9f8fc71b1fb33bb81dd86e0d929d43dc31fc3
riberasandbox <riberasandbox@gmail.com>
2020-09-28 15:36:13 +0200

    Add new conf for sandbox

--- truffle.js
+++ truffle.js
@@ -1,12 +1,12 @@
 const HDWalletProvider = require('truffle-hdwallet-provider');
-const teamsMnemonic = "unaware fine quiz stock level photo vital arrow usage replace impose boss";
+const teamsMnemonic = "exit better neutral theory speak fee crouch spirit mixture verb law symbol";
 
 
 module.exports = {
  networks: {
   development: {
     provider: function() {
-      return new HDWalletProvider(teamsMnemonic, "https://sandbox.truffleteams.com/69a8e39c-caaf-4f4d-9c1e-15f91de9c3a7", 0, 10, false);
+      return new HDWalletProvider(teamsMnemonic, "https://sandbox.truffleteams.com/56c1781e-4114-4bd0-a140-1f123620b410", 0, 10, false);
     },
     port: 443,
     network_id: "*"

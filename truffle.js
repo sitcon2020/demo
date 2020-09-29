b74514807ab901998a847b13efc8dac17cd719c7
riberasandbox <riberasandbox@gmail.com>
2020-09-29 09:54:01 +0200

    New credentials

--- truffle.js
+++ truffle.js
@@ -1,12 +1,12 @@
 const HDWalletProvider = require('truffle-hdwallet-provider');
-const teamsMnemonic = "cherry cricket shrimp usage wing size course buddy scare two parrot lazy";
+const teamsMnemonic = "panther model borrow attitude hat between expand garbage rough month adjust truck";
 
 
 module.exports = {
  networks: {
   development: {
     provider: function() {
-      return new HDWalletProvider(teamsMnemonic, "https://sandbox.truffleteams.com/751cae55-6b87-49b1-9d77-8b7b7cd928a3", 0, 10, false);
+      return new HDWalletProvider(teamsMnemonic, "https://sandbox.truffleteams.com/941380bb-b36d-404d-9fc3-2b543ada078c", 0, 10, false);
     },
     port: 443,
     network_id: "*"
